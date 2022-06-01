03b61b97c5211d9ce350e0a3881e67eb58db725a
Jairo Blatt <jairoblattc@gmail.com>
2022-05-31 22:48:16 -0300

    log

--- services/index.ts
+++ services/index.ts
@@ -4,7 +4,9 @@ const HTTPClient = axios.create({
   baseURL: 'https://dog.ceo/api/breeds/image',
 });
 
+let count = 1;
 export async function getDog(page = 1) {
+  console.log('Fetched =>', { count, page });
   const { data } = await HTTPClient.get('/random');
   return {
     page,
